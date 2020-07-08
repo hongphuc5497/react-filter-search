818beea18980c8f48387e4590a6b8bdf59616675
Hong Phuc <hongphuc.dthp@gmail.com>
2020-07-08 16:29:22 +0700

    Render list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-
-function App() {
-	return <div className="App"></div>;
+import { Information } from "./data";
+class App extends Component {
+	render() {
+		const styleInfo = {
+			paddingRight: "10px",
+    };
+    
+		const items = Information.map((data) => {
+			return (
+				<div>
+					<ul>
+						<li style={{ position: "relative", left: "10vh" }}>
+							<span style={styleInfo}>{data.name}</span>
+							<span style={styleInfo}>{data.age}</span>
+							<span style={styleInfo}>{data.country}</span>
+						</li>
+					</ul>
+				</div>
+			);
+		});
+		return items;
+	}
 }
 
 export default App;
