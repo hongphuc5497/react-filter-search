9679efaa8db2a3117575525388ae774fb9a5c6a4
Hong Phuc <hongphuc.dthp@gmail.com>
2020-07-08 16:56:13 +0700

    Handle search

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React, { Component } from "react";
 import "./App.css";
 import { Information } from "./data";
 class App extends Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      search: null
-    }
-  }
+		this.state = {
+			search: null,
+		};
+	}
 
-  handleSearch = (e) => {
-    this.setState({
-      search: e.target.value
-    })
-  }
+	handleSearch = (e) => {
+		this.setState({
+			search: e.target.value,
+		});
+	};
 
 	render() {
 		const elementStyle = {
@@ -28,13 +28,38 @@ class App extends Component {
 			marginBottom: "10vh",
 		};
 
+		const styleInfo = {
+			paddingRight: "10px",
+		};
+
+		const items = Information.filter((data) => {
+			if (this.state.search == null) {
+				return data;
+			} else if (
+				data.name.toLowerCase().includes(this.state.search.toLowerCase()) ||
+				data.country.toLowerCase().includes(this.state.search.toLowerCase())
+			) {
+				return data;
+			}
+		}).map((data) => (
+			<div>
+				<ul>
+					<li style={{ position: "relative", left: "10vh" }}>
+						<span style={styleInfo}>{data.name}</span>
+						<span style={styleInfo}>{data.age}</span>
+						<span style={styleInfo}>{data.country}</span>
+					</li>
+				</ul>
+			</div>
+		));
+
 		return (
 			<div>
 				<input
 					type="text"
 					style={elementStyle}
-          value={this.state.search}
-          onChange={(e) => this.handleSearch(e)}
+					value={this.state.search}
+					onChange={(e) => this.handleSearch(e)}
 					placeholder="Enter item to be searched"
 				/>
 				{items}
@@ -44,19 +69,3 @@ class App extends Component {
 }
 
 export default App;
-
-const styleInfo = {
-	paddingRight: "10px",
-};
-
-const items = Information.map((data) => (
-	<div>
-		<ul>
-			<li style={{ position: "relative", left: "10vh" }}>
-				<span style={styleInfo}>{data.name}</span>
-				<span style={styleInfo}>{data.age}</span>
-				<span style={styleInfo}>{data.country}</span>
-			</li>
-		</ul>
-	</div>
-));
